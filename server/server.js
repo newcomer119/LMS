5e3950b3b0a3e29d176b1ffb2efaf7044b1b23f2
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-07-16 00:01:28 +0530

    creating full stack lms application

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import clerkWebHooks from './controllers/Webhook.js'
+
+const app = express()
+
+// Middlewares 
+app.use(cors())
+
+// connecting 
+await connectDB()
+
+// routes
+
+app.get('/', (req,res) => res.send("API WORKING"))
+app.post('/clerk', express.json(), clerkWebHooks)
+
+// PORT 
+const PORT = process.env.PORT || 5000 
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
